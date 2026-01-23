1150d9a95302fb9c9934f5b7d9a1c37abcc24db2
Ferenc Deak <ferenc.deak@accesa.eu>
2026-01-23 16:21:03 +0200

    added angular material

--- devjobs-web-app/src/app/header/header.ts
+++ devjobs-web-app/src/app/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
